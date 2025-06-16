b9fc778b608073a8faf90036338ca34408969c21
Veeru <veeru.p@idensys-tech.com>
2025-06-16 20:02:33 +0530

    ip maping

--- src/utils/environment/index.tsx
+++ src/utils/environment/index.tsx
@@ -1,9 +1,8 @@
 import axios from 'axios';
 import localStorageContent from '../localstorage';
-const URL = axios.create({
 
-    //baseURL: process.env.REACT_APP_API_URL
-    baseURL: "http://159.65.159.116/v1/",
+const URL = axios.create({
+    baseURL: process.env.REACT_APP_API_BASE_URL
 })
 
 URL.interceptors.request.use(
