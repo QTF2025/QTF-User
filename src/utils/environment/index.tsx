25cbc31d020a6e067ca78c2664ed69f36b1ed03d
Veeru <veeru.p@idensys-tech.com>
2025-07-10 16:57:32 +0530

    ui

--- src/utils/environment/index.tsx
+++ src/utils/environment/index.tsx
@@ -1,42 +1,42 @@
-import axios from 'axios';
-import localStorageContent from '../localstorage';
-<<<<<<< HEAD
+import axios from "axios";
+import localStorageContent from "../localstorage";
+
 const URL = axios.create({
-    baseURL: process.env.REACT_APP_API_URL
-})
+  baseURL: process.env.REACT_APP_API_BASE_URL,
+});
 
 URL.interceptors.request.use(
-    async (config: any) => {
-        config.headers = {
-            'Authorization': `Bearer ${localStorageContent.getAccessToken()}`
-        }
-        return config
-    },
-    (error: any) => {
-        Promise.reject(error)
-    }
-)
+  async (config: any) => {
+    config.headers = {
+      Authorization: `Bearer ${localStorageContent.getAccessToken()}`,
+    };
+    return config;
+  },
+  (error: any) => {
+    Promise.reject(error);
+  }
+);
 
 URL.interceptors.response.use(
-    (response) => {
-        return response;
-    },
-    (error: any) => {
-        if (error.response) {
-        // If the server responded with an error status code
-        const errorMessage = error.response.data.message || 'An error occurred';
-        if(errorMessage === 'Please authenticate'){
-            localStorage.clear()
-            window.location.href = '/'
-        }
-        return Promise.reject(errorMessage);
-        } else if (error.request) {
-        // If the request was made but no response was received
-        return Promise.reject('No response received');
-        } else {
-        // Something happened in setting up the request
-        return Promise.reject('Request setup error');
-        }
+  (response) => {
+    return response;
+  },
+  (error: any) => {
+    if (error.response) {
+      // If the server responded with an error status code
+      const errorMessage = error.response.data.message || "An error occurred";
+      if (errorMessage === "Please authenticate") {
+        localStorage.clear();
+        window.location.href = "/";
+      }
+      return Promise.reject(errorMessage);
+    } else if (error.request) {
+      // If the request was made but no response was received
+      return Promise.reject("No response received");
+    } else {
+      // Something happened in setting up the request
+      return Promise.reject("Request setup error");
+    }
   }
-)
-export default URL
+);
+export default URL;
